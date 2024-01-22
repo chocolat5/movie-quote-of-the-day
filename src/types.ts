af276fa9a46a60dfd4baf4d15dcdcb8660aca018
chocolat5 <futatsugi.y@gmail.com>
2024-01-22 20:44:32 +0900

    feat: update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,23 @@
-export interface PostItem {
-  id: string;
-  authorName: string;
-  link: string;
+import { CSSProperties } from "react";
+
+export interface Quote {
+  quote: string;
   title: string;
-  date: Date;
+  year: number;
+  size: "small" | "medium" | "large";
 }
+
+export interface TextSize {
+  id: "small" | "medium" | "large";
+  label: "S" | "M" | "L";
+  fontSize: CSSProperties["fontSize"];
+}
+
+export interface Theme {
+  id: string;
+  label: string;
+  color: CSSProperties["color"];
+  bgColor: CSSProperties["backgroundColor"];
+}
+
+export type ClickEvent = React.MouseEvent<HTMLButtonElement>;
