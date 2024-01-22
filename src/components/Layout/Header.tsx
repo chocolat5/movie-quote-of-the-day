4e44845462582a81b4cdb40cbf143e107c23c385
chocolat5 <futatsugi.y@gmail.com>
2024-01-22 20:43:03 +0900

    feat: update Header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,19 +1,24 @@
-import reactLogo from "@/src/assets/react.svg";
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from "/assets/images/vite.svg";
+import { css } from "@emotion/react";
+
+const styles = {
+  header: css`
+    max-width: var(--main);
+    margin: 0 auto;
+    padding: 96px 0 0;
+    text-align: center;
+  `,
+  title: css`
+    font-size: 20px;
+    font-family: "Arial Black", system-ui, Avenir, Helvetica, sans-serif;
+    font-weight: 700;
+    line-height: 1.2;
+  `,
+};
 
 export default function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </>
+    <header css={styles.header}>
+      <h1 css={styles.title}>The Movie Quote of the Day</h1>
+    </header>
   );
 }
