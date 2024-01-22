83aa86590f943f7af551d6738465361343290309
chocolat5 <futatsugi.y@gmail.com>
2024-01-22 20:45:06 +0900

    feat: add sizes and themes to utils

---
+++ src/utils/sizes.ts
@@ -0,0 +1,21 @@
+import { TextSize } from "@/src/types";
+
+const sizes: TextSize[] = [
+  {
+    id: "small",
+    label: "S",
+    fontSize: 20,
+  },
+  {
+    id: "medium",
+    label: "M",
+    fontSize: 26,
+  },
+  {
+    id: "large",
+    label: "L",
+    fontSize: 32,
+  },
+];
+
+export default sizes;
